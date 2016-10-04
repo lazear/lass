c767c13450918fb49c14a3f29e05234da8126ddc
Michael Lazear <lazear@scripps.edu>
2016-10-03 21:59:23 -0700

    Added license to top of file, and minor strcmp->strncmp change

--- assembler.c
+++ assembler.c
@@ -1,3 +1,30 @@
+/*
+  GNU nano 2.7.0                                                                                                   File: LICENSE                                                                                                             
+
+MIT License
+
+Copyright (c) 2016 Michael Lazear
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
 #include <stdio.h>
 #include <stdint.h>
 #include <fcntl.h>
@@ -57,7 +84,6 @@ void make_elf() {
 }
 
 int add_symbol(char* label) {
-//	printf("add symbol: %s @ 0x%x\n", label, current_position);
 	printf("%08x %s:\n", current_position, label);
 	for (int i = 0; i < 32 && i < strlen(label); i++) {
 		symtab[num_symbols].name[i] = label[i];
@@ -85,7 +111,7 @@ int find_symbol(char* label) {
 	for (int i = 0; i < num_symbols; i++)
 		if (strcmp(symtab[i].name, label) == 0)
 			return symtab[i].position + elf_offset;
-	if (strcmp(label, "$$") == 0)
+	if (strncmp(label, "$$", 2) == 0)
 		return 0;
 	if (strcmp(label, "$") == 0)
 		return current_position + elf_offset;
